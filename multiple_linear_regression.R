eea13508595c365aca573fadc34ee78fbfd1d716
Pranav Seth <sethpranav28@gmail.com>
2019-02-22 15:32:50 +0530

    Update multiple_linear_regression.R

--- multiple_linear_regression.R
+++ multiple_linear_regression.R
@@ -1,30 +1,18 @@
-# Multiple Linear Regression
-
-# Importing the dataset
 dataset = read.csv('50_Startups.csv')
 
-# Encoding categorical data
 dataset$State = factor(dataset$State,
                        levels = c('New York', 'California', 'Florida'),
                        labels = c(1, 2, 3))
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Multiple Linear Regression to the Training set
 regressor = lm(formula = Profit ~ .,
                data = training_set)
 
-# Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
 ggplot()+
@@ -36,7 +24,6 @@ ggplot()+
   xlab("R D Spend")+
   ylab("Profit")
 
-#Visualising the test set
 ggplot()+
   geom_point(aes(x=test_set$R.D.Spend, y=test_set$Profit),
              colour='red')+
